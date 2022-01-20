e402803c7adcbd9a1000230ee3113cdffe0535fd
Mateusz Rojek <m.rojek@pureinteractive.pl>
2022-01-20 17:53:11 +0100

    Model for converting currencies

--- src/Models/Currency.ts
+++ src/Models/Currency.ts
@@ -1,3 +1,7 @@
 export default class Currency {
-    
+    static async convert(source, destination) {
+        const res = await fetch(`${process.env.CURRENCY_API_ENDPOINT}/latest?access_key=${process.env.CURRENCY_API_KEY}&base=${source}&symbols=${destination}`);
+        const json = await res.json();
+        return json;
+    }
 }
