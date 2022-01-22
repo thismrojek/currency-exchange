cd1149ccce8e817b99f58ebb3dd75535eb53c2ce
Mateusz Rojek <m.rojek@pureinteractive.pl>
2022-01-22 19:04:36 +0100

    Modified currency's model to get historical data

--- src/Models/Currency.ts
+++ src/Models/Currency.ts
@@ -1,6 +1,6 @@
 export default class Currency {
-    static async convert(source, destination) {
-        const res = await fetch(`${process.env.CURRENCY_API_ENDPOINT}/latest?access_key=${process.env.CURRENCY_API_KEY}&base=${source}&symbols=${destination}`);
+    static async convert(source: string, destination: string, data?: string) {
+        const res = await fetch(`${process.env.CURRENCY_API_ENDPOINT}/${data ? data : 'latest'}?access_key=${process.env.CURRENCY_API_KEY}&base=${source}&symbols=${destination}`);
         const json = await res.json();
         return json;
     }
